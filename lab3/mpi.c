573d899b5b44b032e657694f0b816886d977caed
WajHub <hubert.wajda03@gmail.com>
2026-03-14 21:32:14 +0100

    refactor

--- lab3/mpi.c
+++ lab3/mpi.c
@@ -75,15 +75,6 @@ double countTwinPrimes(long a, long b, long upper_limit)
     }
   }
   return count;
-
-  // if(a%2== 0) a++;
-  // for (long i = a; i <= b; i+=2) {
-  //     if (isPrime(i) && isPrime(i + 2)) {
-  //       printf(" ---- Twin Primes: %ld %ld \n", i, i+2);
-  //       count++;
-  //     }
-  // }
-  return count;
 }
 
 int main(int argc, char **argv)
@@ -96,7 +87,6 @@ int main(int argc, char **argv)
   double b = ins__args.arg;
   double range[2];
   double result = 0, resulttemp;
-  int sentcount = 0;
   int i;
   MPI_Status status;
 
@@ -104,15 +94,13 @@ int main(int argc, char **argv)
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   MPI_Comm_size(MPI_COMM_WORLD, &proccount);
-  if (proccount < 2)
-  {
+  if (proccount < 2){
     printf("Run with at least 2 processes");
     MPI_Finalize();
     return -1;
   }
 
-  if (((b - a) / RANGESIZE) < 2 * (proccount - 1))
-  {
+  if (((b - a) / RANGESIZE) < 2 * (proccount - 1)){
     printf("More subranges needed");
     MPI_Finalize();
     return -1;
@@ -120,38 +108,34 @@ int main(int argc, char **argv)
 
   SlaveHandler *handlers = malloc(sizeof(SlaveHandler) * (proccount - 1));
   MPI_Request *requests = malloc(sizeof(MPI_Request) * (proccount - 1));
-  if (handlers == NULL || requests == NULL)
-  {
+  if (handlers == NULL || requests == NULL){
     perror("malloc");
     return -1;
   }
 
   // master will distribute the data and slave processes will perform computations
-  if (myrank == 0)
-  {
+  if (myrank == 0){
     gettimeofday(&ins__tstart, NULL);
     range[0] = a;
 
-    // INITAL SENDING - first distribute some ranges to all slaves
-    for (i = 1; i < proccount; i++)
-    {
+    // INITAL SENDING
+    for (i = 1; i < proccount; i++){
       int handler_index = i - 1;
       range[1] = range[0] + RANGESIZE - 1;
       MPI_Send(range, 2, MPI_DOUBLE, i, DATA, MPI_COMM_WORLD); // send it to process i
-      sentcount++;
 
       MPI_Irecv(&handlers[handler_index].result, 1, MPI_DOUBLE, i, RESULT, MPI_COMM_WORLD, &handlers[handler_index].recv_req);
       range[0] = range[1] + 1;
     }
 
-    for (i = 1; i < proccount; i++)
-    {
+    // SENDING next Non-blocking messages
+    for (i = 1; i < proccount; i++){
       int handler_index = i - 1;
-      if (range[0] <= b)
-      {
+      if (range[0] <= b){
         range[1] = range[0] + RANGESIZE - 1;
-        if (range[1] > b)
+        if (range[1] > b){
           range[1] = b;
+        }
 
         handlers[handler_index].range[0] = range[0];
         handlers[handler_index].range[1] = range[1];
@@ -159,153 +143,112 @@ int main(int argc, char **argv)
         MPI_Isend(handlers[handler_index].range, 2, MPI_DOUBLE, i, DATA, MPI_COMM_WORLD, &handlers[handler_index].send_req);
         range[0] = range[1] + 1;
       }
-      else
-      {
+      else{
         handlers[handler_index].send_req = MPI_REQUEST_NULL;
       }
     }
 
     // MASTER MAINLOOP
-    while (range[0] <= b)
-    {
-      int completed_idx;
+    while (range[0] <= b){
+      int completed_index;
 
-      // Kopiujemy żądania odbioru do tablicy dla Waitany
-      for (int j = 0; j < proccount - 1; j++)
+      for (int j = 0; j < proccount - 1; j++){
         requests[j] = handlers[j].recv_req;
+      }
 
-      // Czekamy na dowolny wynik
-      MPI_Waitany(proccount - 1, requests, &completed_idx, &status);
+      // Wait for result
+      MPI_Waitany(proccount - 1, requests, &completed_index, &status);
 
-      int target_slave = completed_idx + 1;
-      result += handlers[completed_idx].result; // Dodajemy odebrany wynik [cite: 126]
+      int target_slave = completed_index + 1;
+      result += handlers[completed_index].result; // Add part-resutl to Final Result
 
-      // Upewniamy się, że poprzedni Isend (zapas) do tego Slave'a wyszedł z bufora [cite: 133]
-      MPI_Wait(&handlers[completed_idx].send_req, MPI_STATUS_IGNORE);
+      // Ensure - previoues messagesd was finished
+      MPI_Wait(&handlers[completed_index].send_req, MPI_STATUS_IGNORE);
 
-      // Przygotowujemy i wysyłamy kolejny zapas [cite: 135, 144]
       range[1] = range[0] + RANGESIZE - 1;
-      if (range[1] > b)
+      if (range[1] > b){
         range[1] = b;
+      }
+        
+      handlers[completed_index].range[0] = range[0];
+      handlers[completed_index].range[1] = range[1];
 
-      handlers[completed_idx].range[0] = range[0];
-      handlers[completed_idx].range[1] = range[1];
+      MPI_Isend(handlers[completed_index].range, 2, MPI_DOUBLE, target_slave, DATA, MPI_COMM_WORLD, &handlers[completed_index].send_req);
 
-      MPI_Isend(handlers[completed_idx].range, 2, MPI_DOUBLE, target_slave, DATA, MPI_COMM_WORLD, &handlers[completed_idx].send_req);
-
-      // Odświeżamy Irecv, by czekać na wynik z tego, co Slave liczy TERAZ [cite: 148]
-      MPI_Irecv(&handlers[completed_idx].result, 1, MPI_DOUBLE, target_slave, RESULT, MPI_COMM_WORLD, &handlers[completed_idx].recv_req);
+      // Refresh Irecv to read new data
+      MPI_Irecv(&handlers[completed_index].result, 1, MPI_DOUBLE, target_slave, RESULT, MPI_COMM_WORLD, &handlers[completed_index].recv_req);
 
       range[0] = range[1] + 1;
     }
 
-    // do
-    // {
-    //   // distribute remaining subranges to the processes which have completed their parts
-    //   MPI_Recv(&resulttemp, 1, MPI_DOUBLE, MPI_ANY_SOURCE, RESULT, MPI_COMM_WORLD, &status);
-    //   result += resulttemp;
-    //   // check the sender and send some more data
-    //   range[1] = range[0] + RANGESIZE - 1;
-    //   if (range[1] > b)
-    //     range[1] = b;
-    //   MPI_Send(range, 2, MPI_DOUBLE, status.MPI_SOURCE, DATA,
-    //            MPI_COMM_WORLD);
-    //   range[0] = range[1] + 1;
-    // } while (range[1] < b);
-
-    // // now receive results from the processes
-    // for (i = 0; i < (proccount - 1); i++)
-    // {
-    //   MPI_Recv(&resulttemp, 1, MPI_DOUBLE, MPI_ANY_SOURCE, RESULT, MPI_COMM_WORLD, &status);
-    //   result += resulttemp;
-    // }
-    // // shut down the slaves
-    // for (i = 1; i < proccount; i++)
-    // {
-    //   MPI_Send(NULL, 0, MPI_DOUBLE, i, FINISH, MPI_COMM_WORLD);
-    // }
-
-    // ... po pętli while (range[0] <= b) ...
-
-    // 1. Musimy odebrać wyniki dla wszystkich otwartych żądań Irecv
-    for (i = 0; i < proccount - 1; i++)
-    {
+    for (i = 0; i < proccount - 1; i++){
       MPI_Wait(&handlers[i].recv_req, MPI_STATUS_IGNORE);
       result += handlers[i].result;
     }
 
-    // 2. Musimy odebrać wyniki za te "zapasowe" zadania, które Slave'y liczą TERAZ
-    // Każdy Slave dostał jeszcze jedno zadanie przez Isend, za które NIE wystawiliśmy jeszcze Irecv w pętli while
-    for (i = 1; i < proccount; i++)
-    {
+    for (i = 1; i < proccount; i++){
       MPI_Recv(&resulttemp, 1, MPI_DOUBLE, i, RESULT, MPI_COMM_WORLD, &status);
       result += resulttemp;
     }
 
-    // 3. Dopiero teraz zamykamy Slave'y
-    for (i = 1; i < proccount; i++)
-    {
+    // Finish (after receiving all messages)
+    for (i = 1; i < proccount; i++){
       MPI_Send(NULL, 0, MPI_DOUBLE, i, FINISH, MPI_COMM_WORLD);
     }
     gettimeofday(&ins__tstop, NULL);
     ins__printtime(&ins__tstart, &ins__tstop, ins__args.marker);
   }
 
-  else
-  {
+  else{
     double next_range[2];
-    double results[2]; // Bufor na dwa wyniki
+    double results[2];
     int res_idx = 0;
     MPI_Request recv_req = MPI_REQUEST_NULL;
     MPI_Request send_req = MPI_REQUEST_NULL;
 
     MPI_Recv(range, 2, MPI_DOUBLE, 0, DATA, MPI_COMM_WORLD, &status);
 
-    while (status.MPI_TAG != FINISH)
-    {
+    while (status.MPI_TAG != FINISH){
       MPI_Irecv(next_range, 2, MPI_DOUBLE, 0, MPI_ANY_TAG, MPI_COMM_WORLD, &recv_req);
 
       results[res_idx] = countTwinPrimes(range[0], range[1], b);
 
-      if (send_req != MPI_REQUEST_NULL)
+      if (send_req != MPI_REQUEST_NULL){
         MPI_Wait(&send_req, MPI_STATUS_IGNORE);
+      }
 
-      // Wysyłamy z bezpiecznego bufora results[res_idx]
       MPI_Isend(&results[res_idx], 1, MPI_DOUBLE, 0, RESULT, MPI_COMM_WORLD, &send_req);
 
-      res_idx = 1 - res_idx; // Przełączamy bufor (0 -> 1, 1 -> 0)
+      if(res_idx == 0 ){
+        res_idx = 1;
+      }
+      else {
+        res_idx = 0;
+      }
 
       MPI_Wait(&recv_req, &status);
       range[0] = next_range[0];
       range[1] = next_range[1];
     }
-    // Na koniec upewnij się, że ostatni Isend wyszedł
-    if (send_req != MPI_REQUEST_NULL)
+
+    if (send_req != MPI_REQUEST_NULL){
       MPI_Wait(&send_req, MPI_STATUS_IGNORE);
+    }
+      
   }
-
-  // else {
-  //   // SLAVE MAINLOOP
-  //   do
-  //   {
-  //     MPI_Probe(0, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
-
-  //     if (status.MPI_TAG == DATA)
-  //     {
-  //       MPI_Recv(range, 2, MPI_DOUBLE, 0, DATA, MPI_COMM_WORLD, &status);
-  //       // compute my part
-  //       resulttemp = countTwinPrimes(range[0], range[1], b);
-  //       // send the result back
-  //       MPI_Send(&resulttemp, 1, MPI_DOUBLE, 0, RESULT, MPI_COMM_WORLD);
-  //     }
-  //   } while (status.MPI_TAG != FINISH);
-  // }
 
   if (!myrank)
   {
     printf("\nFinal Result %f \n", result);
   }
 
+  free(handlers);
+  free(requests);
+
   MPI_Finalize();
   return 0;
 }
+
+
+
+// Master wysyła pierwsze porcje zadań, a zaraz po tym wysyła nieblokująco "zapasowe" porcje
